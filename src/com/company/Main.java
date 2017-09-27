cf2733fe69e022e37e6d579af10adf2cb97c8e75
javaDeveloperEgor <developerEgor@yandex.ru>
2017-09-27 23:41:47 +0300

    lab2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void print(String[] args){
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
     }
+
+    public static void sort(String[] args){
+        Arrays.sort(args, new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                return str1.compareTo(str2);
+            }
+        });
+
+        System.out.println(Arrays.toString(args));
+    }
+
+    public static void main(String[] args) {
+        String[] arrayString = new String[]{"Kyiv", "Warshawa", "Amsterdam"};
+        print(arrayString);
+        System.out.println();
+        sort(arrayString);
+    }
+
 }
